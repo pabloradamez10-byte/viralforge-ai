f699fb6c7b0b7a3bf4255614060f192fd6273941
pabloradamez10-byte <pabloradamez10@gmail.com>
2026-07-07 20:23:35 -0300

    Add files via upload

---
+++ backend/jest.config.js
@@ -0,0 +1,16 @@
+/** @type {import('jest').Config} */
+export default {
+  preset: 'ts-jest/presets/default-esm',
+  testEnvironment: 'node',
+  extensionsToTreatAsEsm: ['.ts'],
+  moduleNameMapper: {
+    '^@/(.*)$': '<rootDir>/src/$1',
+    '^(\\.{1,2}/.*)\\.js$': '$1',
+  },
+  transform: {
+    '^.+\\.ts$': ['ts-jest', { useESM: true }],
+  },
+  testMatch: ['**/__tests__/**/*.spec.ts'],
+  collectCoverageFrom: ['src/**/*.ts', '!src/**/*.d.ts', '!src/server.ts'],
+  setupFiles: ['<rootDir>/tests/jest.setup.ts'],
+};
